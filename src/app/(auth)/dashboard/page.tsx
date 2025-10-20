b425fa47329b41362de48154f6eecb077dfbb2ea
tanzid109 <tanzid.unkhalil109@gmail.com>
2025-10-21 04:01:49 +0600

    dashboard preview basic structure

--- src/app/(auth)/dashboard/page.tsx
+++ src/app/(auth)/dashboard/page.tsx
@@ -2,9 +2,29 @@ import React from 'react';
 
 const page = () => {
     return (
-        <div>
-            Dashboard
-        </div>
+        <main className="p-3">
+            <div className="grid grid-cols-1 gap-4 min-h-screen md:grid-cols-6 md:grid-rows-6">
+                {/* Welcome section */}
+                <div className="bg-amber-400 rounded-2xl p-4 md:col-span-2 md:row-span-2">1</div>
+
+                {/* Customer and total earning */}
+                <div className="bg-amber-400 rounded-2xl p-4 md:col-start-1 md:row-start-3 md:row-span-2">2</div>
+                <div className="bg-amber-400 rounded-2xl p-4 md:col-start-2 md:row-start-3 md:row-span-2">3</div>
+
+                {/* Performance chart */}
+                <div className="bg-amber-400 rounded-2xl p-4 md:col-span-4 md:row-span-4 md:col-start-3 md:row-start-1">4</div>
+
+                {/* Performance history */}
+                <div className="bg-amber-400 rounded-2xl p-4 md:col-span-2 md:row-span-2 md:row-start-5">5</div>
+
+                {/* Performance line */}
+                <div className="bg-amber-400 rounded-2xl p-4 md:col-span-2 md:row-span-2 md:col-start-3 md:row-start-5">6</div>
+
+                {/* Sales progress */}
+                <div className="bg-amber-400 rounded-2xl p-4 md:col-span-2 md:row-span-2 md:col-start-5 md:row-start-5">7</div>
+            </div>
+        </main>
+
     );
 };
 
